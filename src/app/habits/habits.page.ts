f6b1f31d5e18bb41d7927bcf2a5a3489f8162f36
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 22:35:19 +0100

    change newHabit into actual habit object, add methods for adding and removing habits, confirming modal

--- src/app/habits/habits.page.ts
+++ src/app/habits/habits.page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption, ModalController, IonList, IonItemSliding } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption, ModalController, IonList, IonItemSliding, IonDatetimeButton, IonPopover, IonItemOptions, IonItemOption, IonIcon } from '@ionic/angular/standalone';
 import { HabitComponent } from '../habit/habit.component';
 import { Habit } from '../habit/habit.model';
 
@@ -9,33 +9,57 @@ import { Habit } from '../habit/habit.model';
   templateUrl: 'habits.page.html',
   styleUrls: ['habits.page.scss'],
   standalone: true,
-  imports: [IonItemSliding, IonList, HabitComponent, FormsModule, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
+  imports: [IonIcon, IonItemOption, IonItemOptions, IonPopover, IonDatetimeButton, IonItemSliding, IonList, HabitComponent, FormsModule, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
 })
 export class HabitsPage {
   @ViewChild(IonModal) modal: IonModal; // get the modal component
 
-  habits: Array<HabitComponent>; // array of habits
+  habits: Habit[] = []; // array of habits
 
   // categories for the select input
   categories = ['Health', 'Finance', 'Career', 'Personal Growth', 'Relationships', 'Other'];
-  newHabit = {
-    id: 0,
-    name: '',
-    description: '',
-    frequency: '',
-    goal: 0,
-    current: 0,
-    deadline: new Date(),
-    category: ''
-  };
+
+  // new habit object from model
+  habitToBeAdded = new Habit(0, '', '', 0, 0, new Date(), '');
 
   cancelModal() { // close the modal
     this.modal.dismiss();
   }
 
+  confirmModal() {
+    this.addHabit();
+    this.modal.dismiss();
+  }
+
+  addHabit() {
+    // set the id of the new habit
+    this.habitToBeAdded.id = this.habits.length;
+    // add the new habit to the array
+    this.habits.push(this.habitToBeAdded);
+    // close the modal
+    this.modal.dismiss();
+    // save the habits to local storage
+    localStorage.setItem('habits', JSON.stringify(this.habits));
+    console.log(this.habits);
+  }
+
+  deleteHabit(id: number) {
+    // filter out the habit with the id to be deleted
+    this.habits = this.habits.filter(habit => habit.id !== id);
+    // save the habits to local storage
+    localStorage.setItem('habits', JSON.stringify(this.habits));
+  }
+
   constructor() {
     // get the habits from local storage, else set to empty array
     // let habits = localStorage.getItem('habits') || '[]';
+  }
+
+  ngOnInit() {
+    // get the habits from local storage, else set to empty array
+    let habits = localStorage.getItem('habits') || '[]';
+    // parse the habits to JSON and set to the habits array
+    this.habits = JSON.parse(habits);
   }
 
 }
