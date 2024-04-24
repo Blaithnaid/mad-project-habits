9349cdea1390b41ac792f20305c0b3219cc45ba0
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 03:11:25 +0100

    filling out habits page

--- src/app/habits/habits.page.ts
+++ src/app/habits/habits.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { CreateHabitComponent } from '../create-habit/create-habit.component';
 
 @Component({
   selector: 'app-habits',
   templateUrl: 'habits.page.html',
   styleUrls: ['habits.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [CreateHabitComponent, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
 })
 export class HabitsPage {
 
