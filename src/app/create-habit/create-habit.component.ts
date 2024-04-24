2ce2e1ac11635d3a6cc4c2d320e68c98a855450b
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 03:11:14 +0100

    add component to use in modal

---
+++ src/app/create-habit/create-habit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-habit',
+  templateUrl: './create-habit.component.html',
+  styleUrls: ['./create-habit.component.scss'],
+  standalone: true
+})
+export class CreateHabitComponent  implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
