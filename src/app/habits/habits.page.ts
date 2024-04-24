d6e26d56bc84d6dfa1234c27ae7bbff88880c65e
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 14:43:02 +0100

    adding controls for modal, moved contents to app-habit component for now

--- src/app/habits/habits.page.ts
+++ src/app/habits/habits.page.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption, ModalController } from '@ionic/angular/standalone';
 import { CreateHabitComponent } from '../create-habit/create-habit.component';
+import { HabitComponent } from '../habit/habit.component';
 
 @Component({
   selector: 'app-habits',
   templateUrl: 'habits.page.html',
   styleUrls: ['habits.page.scss'],
   standalone: true,
-  imports: [CreateHabitComponent, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
+  imports: [CreateHabitComponent, HabitComponent, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
 })
 export class HabitsPage {
+  @ViewChild(IonModal) modal: IonModal;
 
-  constructor() {}
+  habits: Array<HabitComponent>;
+
+  constructor() {
+    // get the habits from local storage, else set to empty array
+    // let habits = localStorage.getItem('habits') || '[]';
+  }
 
 }
