048b05f6a181633858b967fa5ec2a0998c1df16d
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 19:31:25 +0100

    removing picture

--- src/app/habit/habit.model.ts
+++ src/app/habit/habit.model.ts
@@ -5,16 +5,14 @@ export class Habit {
     goal: number;
     current: number;
     deadline: Date;
-    picture: string;
     category: string;
-    constructor(id: number, name: string, description: string, goal: number, current: number, deadline: Date, picture: string, category: string) {
+    constructor(id: number, name: string, description: string, goal: number, current: number, deadline: Date, category: string) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.goal = goal;
         this.current = current;
         this.deadline = deadline;
-        this.picture = picture;
         this.category = category;
     }
 }
