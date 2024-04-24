acf51a4e4fc4e677cf0b542441e97de75587c9e1
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 17:58:40 +0100

    add deadline and picture to habit model, add newHabit object to use for inserting data from modal

--- src/app/habit/habit.model.ts
+++ src/app/habit/habit.model.ts
@@ -5,11 +5,14 @@ export class Habit {
     goal: number;
     current: number;
     deadline: Date;
-    constructor(id: number, name: string, description: string, goal: number, current: number, deadline: Date) {
+    picture: string;
+    constructor(id: number, name: string, description: string, goal: number, current: number, deadline: Date, picture: string) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.goal = goal;
         this.current = current;
+        this.deadline = deadline;
+        this.picture = picture;
     }
 }
--- src/app/habits/habits.page.ts
+++ src/app/habits/habits.page.ts
@@ -1,19 +1,35 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonModal, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption, ModalController } from '@ionic/angular/standalone';
-import { CreateHabitComponent } from '../create-habit/create-habit.component';
 import { HabitComponent } from '../habit/habit.component';
+import { Habit } from '../habit/habit.model';
 
 @Component({
   selector: 'app-habits',
   templateUrl: 'habits.page.html',
   styleUrls: ['habits.page.scss'],
   standalone: true,
-  imports: [CreateHabitComponent, HabitComponent, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
+  imports: [HabitComponent, FormsModule, IonTextarea, IonInput, IonModal, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption]
 })
 export class HabitsPage {
   @ViewChild(IonModal) modal: IonModal;
 
   habits: Array<HabitComponent>;
+
+  // newHabit: Habit;
+  newHabit = {
+    id: 0,
+    name: '',
+    description: '',
+    goal: 0,
+    current: 0,
+    deadline: new Date(),
+    picture: ''
+  };
+
+  cancelModal() {
+    this.modal.dismiss();
+  }
 
   constructor() {
     // get the habits from local storage, else set to empty array
