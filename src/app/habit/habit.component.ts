f18085fbfadacb033a2dc60c6146f1fef07c75ef
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-24 22:33:34 +0100

    adjust imports

--- src/app/habit/habit.component.ts
+++ src/app/habit/habit.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { IonTitle } from "@ionic/angular/standalone";
+import { IonTitle, IonContent, IonItem, IonLabel, IonInput, IonTextarea } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-habit',
   templateUrl: './habit.component.html',
   styleUrls: ['./habit.component.scss'],
   standalone: true,
-  imports: [IonTitle, ]
+  imports: [IonTextarea, IonInput, IonLabel, IonItem, IonContent, IonTitle, ]
 })
 export class HabitComponent  implements OnInit {
 
