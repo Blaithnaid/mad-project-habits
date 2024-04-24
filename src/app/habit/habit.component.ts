791893090d47d712a626594943d161f60fdeee65
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-25 00:25:28 +0100

    refactoring to move modal into own component

--- src/app/habit/habit.component.ts
+++ src/app/habit/habit.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { IonTitle, IonContent, IonItem, IonLabel, IonInput, IonTextarea } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonButtons, IonButton, IonItem, IonLabel, IonInput, IonTextarea } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Habit } from './habit.model';
 
 @Component({
   selector: 'app-habit',
   templateUrl: './habit.component.html',
   styleUrls: ['./habit.component.scss'],
   standalone: true,
-  imports: [IonTextarea, IonInput, IonLabel, IonItem, IonContent, IonTitle, ]
+  imports: [IonTextarea, IonInput, IonLabel, IonItem, IonCardTitle, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardContent, IonCardSubtitle, IonButtons, IonButton]
 })
-export class HabitComponent  implements OnInit {
+export class HabitComponent implements OnInit {
+  @Input(): habit: Habit;
 
   constructor() { }
 
