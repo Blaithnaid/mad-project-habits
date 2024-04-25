a785f9a484c2f442d61e6a40d58da4feb8e568c6
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-25 03:46:53 +0100

    idk some tidying up

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonButtons, IonButton, IonAvatar, IonItem, IonLabel, IonList, IonProgressBar } from '@ionic/angular/standalone';
 import { QuotesAPIService } from '../quotes-api.service';
 
 @Component({
@@ -7,22 +7,20 @@ import { QuotesAPIService } from '../quotes-api.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardContent, IonCardSubtitle, IonButtons, IonButton
+  imports: [IonProgressBar, IonList, IonLabel, IonItem, IonAvatar, IonCardTitle, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardContent, IonCardSubtitle, IonButtons, IonButton
   ]
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor(private quotesAPI: QuotesAPIService) {
     this.quotesAPI = quotesAPI;
   }
-
 
   dailyQuoteText: any;
   dailyQuoteAuthor: any;
 
   ngOnInit() {
+    // get a quote from the quotes service
     this.quotesAPI.getQuote().subscribe(data => {
-      // this API is down at the moment
-      // this.dailyQuoteText = data.q;
       this.dailyQuoteAuthor = data.author;
       this.dailyQuoteText = data.content
     });
